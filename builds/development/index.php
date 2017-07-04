0d8b0ad73d255745afdaefc25cad0fe40373f4f9
Mohajireen Miraj <mohajireen.miraj@ifsworld.com>
2017-07-04 08:15:08 +0530

    sidenav one

---
+++ builds/development/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+     <div id="wrapper" class="toggled">
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        SriLankanKey
+                    </a>
+                </li>
+                <li>
+                    <strong><a href="#">Home</a></strong>
+                    
+                </li>
+                <li>
+                    <strong><a href="#">Our Projects</a></strong>
+                </li>
+                <li>
+                    <strong><a href="#">Social Service</a></strong>
+                </li>               
+                <li>
+                    <strong><a href="#">About Us</a></strong>
+                </li>
+                <li>
+                    <strong><a href="#">Donate</a></strong>
+                </li>                
+            </ul>
+            
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Simple Sidebar</h1>
+                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script> 
+<script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+  crossorigin="anonymous"></script>        
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<script src="js/script.js"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+</script>
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+var gulp=require ('gulp'),
+    gutil=require('gulp-util'),
+    concat=require('gulp-concat'),
+    browserify=require('gulp-browserify'),
+    connect = require('gulp-connect-php'),
+    browserSync = require('browser-sync').create();
+    
+
+var jsSources,outputDir;
+var reload  = browserSync.reload;
+
+jsSources='components/scripts/*.js';
+outputDir = 'builds/development/';
+phpSources= 'builds/development/*.php';
+htmlSources= 'builds/development/views/*.html';
+includeSources= 'builds/development/includes/*.inc.php';
+cssSources='components/css/*.css'
+
+gulp.task('log',function(){    
+    gutil.log("Workflows are awesome");
+});
+
+//js task
+gulp.task('js',function(){
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest(outputDir+'js'))        
+});
+
+
+//css task
+gulp.task('css',function(){
+    gulp.src(cssSources)
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(outputDir+'css'))
+});
+
+
+//php task
+gulp.task('php',function(){
+    gulp.src(phpSources);
+});
+
+//html task for views
+gulp.task('html',function(){
+    gulp.src(htmlSources);
+});
+
+gulp.task('includes',function(){
+    gulp.src(includeSources);
+});
+
+//watch task
+gulp.task('watch',function(){
+    gulp.watch(jsSources,['js-watch']);
+    gulp.watch(phpSources,['php-watch']);
+    gulp.watch(htmlSources,['html-watch']);
+    gulp.watch(includeSources,['include-watch']);
+    gulp.watch(cssSources,['css-watch']);
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+    
+// create a task that ensures the `css` task is complete before
+// reloading browsers
+gulp.task('css-watch', ['css'], function (done) {
+    browserSync.reload();
+    done();
+});    
+
+// create a task that ensures the `php` task is complete before
+// reloading browsers
+gulp.task('php-watch', ['php'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+// create a task that ensures the `html` task is complete before
+// reloading browsers
+gulp.task('html-watch', ['html'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+// create a task that ensures the `include` task is complete before
+// reloading browsers
+gulp.task('include-watch', ['includes'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+
+gulp.task('browserSync',['connect'], function() {
+    browserSync.init({
+        proxy:'127.0.0.1:8010',
+        port:8080,
+        open: true,
+        notify :false
+    });
+});
+
+gulp.task('connect', function() {
+  connect.server({base : 'builds/development',                  
+                  port : 8010,
+                  keepalive: true
+                 });
+});
+
+
+
+gulp.task('default',['js','css','watch','browserSync']);    
+
+
